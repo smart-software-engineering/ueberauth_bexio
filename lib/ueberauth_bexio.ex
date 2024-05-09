2aa69e56fec307b5b44e76f114c7f5be28e93e12
Rico Metzger <rico.metzger@gmail.com>
2024-05-09 14:02:10 +0200

    Initial Mix

---
+++ lib/ueberauth_bexio.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthBexio do
+  @moduledoc """
+  Documentation for `UeberauthBexio`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthBexio.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_bexio_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthBexioTest do
+  use ExUnit.Case
+  doctest UeberauthBexio
+
+  test "greets the world" do
+    assert UeberauthBexio.hello() == :world
+  end
+end
